629bb01ae7ac1563e3a73b3b4917f8e8e91158cc
unknown <aaradhya.korde@gmail.com>
2023-08-16 13:40:59 +0530

    Shifted sha512 back to sha 256;Added blockchain

--- crypto-hash.js
+++ crypto-hash.js
@@ -1,7 +1,7 @@
 const crypto = require("crypto");
 
 const cryptoHash = (...inputs) => {
-  const hash = crypto.createHash("sha512");
+  const hash = crypto.createHash("sha256");
 
   hash.update(inputs.sort().join(" "));
 
--- crypto-hash.test.js
+++ crypto-hash.test.js
@@ -3,7 +3,7 @@ const cryptoHash = require("./crypto-hash");
 describe("cryptoHash()", () => {
   it("generates a SHA-512 hashed output", () => {
     expect(cryptoHash("foo"))
-      .toEqual("f7fbba6e0636f890e56fbbf3283e524c6fa3204ae298382d624741d0dc6638326e282c41be5e4254d8820772c5518a2c5a8c0c7f7eda19594a7eb539453e1ed7");
+      .toEqual("2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae");
   });
 
   it("produces the same hash with the same input arguments in any order", () => {
