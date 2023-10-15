a62687ab31eb002be21fea2973e34fa08484d767
rudrakshk-101 <rudrakshkumrawat110@gmail.com>
2023-10-15 19:35:22 +0530

    commit check 2

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 
 render(
-    <div>Cryptochain in React!: Third Live Updateee</div>,
+    <div>Cryptochain in React!: Third Live Updat</div>,
     document.getElementById('root')
 );
